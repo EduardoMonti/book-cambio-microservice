f73331192dc0e46618bf1aa3ea97ed641d1931ed
Carlos Eduardo <dudumonti@hotmail.com>
2024-01-28 22:32:16 -0300

    environment depencie in cambioController, heidi and flyway dependencie

--- cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
@@ -1,6 +1,9 @@
 package br.com.erudio.controller;
 
 import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +16,16 @@ import br.com.erudio.model.Cambio;
 public class CambioController {
  
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping(value = "{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable("amount")BigDecimal amount, @PathVariable("from") String from,  @PathVariable("from") String to) {
 		
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		
+		var port = environment.getProperty("local.server.port");
+		
+		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
