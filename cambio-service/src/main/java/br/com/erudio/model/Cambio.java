e532311bd5b1cbc8b61af61c1ef1f21b166c1989
Carlos Eduardo <dudumonti@hotmail.com>
2024-01-30 16:26:19 -0300

    cambio service and cambio controller changes

--- cambio-service/src/main/java/br/com/erudio/model/Cambio.java
+++ cambio-service/src/main/java/br/com/erudio/model/Cambio.java
@@ -4,13 +4,35 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+
+
+@Entity(name = "cambio")
 public class Cambio implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "from_currency", nullable = false, length = 255)
 	private String from;
+	
+	@Column(name = "to_currency", nullable = false, length = 255)
 	private String to;
+	
+	@Column(nullable = false)
 	private BigDecimal conversionFactor;
+	
+	@Transient
 	private BigDecimal convertedValue;
+	
+	@Transient
 	private String environment;
 	
 	
